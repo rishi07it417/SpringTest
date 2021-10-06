f21066100108919ad042bcaf8c5470ae9232bf81
Rishi Pomal <38519496+rishi07it417@users.noreply.github.com>
2021-10-06 18:13:58 +0530

    updates

--- fileUploader/src/main/java/com/test/fileUploader/ServletInitializer.java
+++ fileUploader/src/main/java/com/test/fileUploader/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//test
 		return application.sources(FileUploaderApplication.class);
 	}
 
